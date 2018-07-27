2b2df16d010c6d373d9aa334fd77e3905aafd34a
qmxp55 <omar.a.ruiz-macias@durham.ac.uk>
2018-07-27 11:20:31 +0100

    adding polybar to multiple screens

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,4 +9,12 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 polybar example &
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
 echo "Bars launched..."
